50e3e303627720326132825a2d1e554652704e22
Gerald <gerald.muecke@gmail.com>
2014-11-24 21:06:23 +0100

    HAL-5 added JAX-RS support for writing JSON and XML Representations for halite resources

---
+++ halite-rs/src/main/java/li/moskito/halite/rs/ClasspathUriResolver.java
@@ -0,0 +1,84 @@
+package li.moskito.halite.rs;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.URIResolver;
+import javax.xml.transform.stream.StreamSource;
+
+class ClasspathUriResolver implements URIResolver {
+
+    private final Class<?> baseClass;
+    private String basePath;
+
+    /**
+     * Creates an {@link URIResolver} that starts searching at the classpath root.
+     * @throws ParserConfigurationException
+     */
+    public ClasspathUriResolver() throws ParserConfigurationException {
+        this(ClasspathUriResolver.class);
+    }
+    
+    /**
+     * Creates an {@link URIResolver} that starts searching at the classpath root
+     * @param base
+     * @throws ParserConfigurationException
+     */
+    public ClasspathUriResolver(String base) throws ParserConfigurationException {
+        this(ClasspathUriResolver.class, base);
+    }
+    
+    /**
+     * Creates an {@link URIResolver} that starts searching at the classpath root
+     * @param base
+     * @throws ParserConfigurationException
+     */
+    public ClasspathUriResolver(final Class<?> base) throws ParserConfigurationException {
+        this(base, "/");
+    }
+    
+    /**
+     * Creates an {@link URIResolver} that starts searching at the given basePath of the classpath
+     * @param base
+     * @param basePath
+     *  the path prefix to resolve resources
+     * @throws ParserConfigurationException
+     */
+    public ClasspathUriResolver(final Class<?> base, String basePath) throws ParserConfigurationException {
+        this.baseClass = base;
+        this.basePath = basePath;
+    }
+
+    @Override
+    public Source resolve(final String href, final String base)
+            throws TransformerException {
+
+        final URL resource = resolve(href);
+        if(resource != null){
+            try {
+                return new StreamSource(resource.openStream());
+            } catch (IOException e) {
+               throw new TransformerException("Could access resource " + href, e);
+            }
+        } else {
+            throw new RuntimeException("Could not resolve resource " + href);
+        }
+
+    }
+    
+    public URL resolve(String name){
+        String fullResourceName = this.basePath + name;
+        final ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        URL url = null;
+        if (cl != null) {
+            url = cl.getResource(fullResourceName);
+        } 
+        if(url == null) {
+            url = baseClass.getResource(fullResourceName);
+        }
+        return url;
+    }
+}
---
+++ halite-rs/src/main/java/li/moskito/halite/rs/OutputTransformMessageBodyWriter.java
@@ -0,0 +1,89 @@
+package li.moskito.halite.rs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.util.JAXBSource;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import li.moskito.halite.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class OutputTransformMessageBodyWriter extends HaliteMessageBodyWriter<Resource> {
+
+    /**
+     * SLF4J Logger for this class
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(OutputTransformMessageBodyWriter.class);
+
+    private Transformer transformer;
+
+    @PostConstruct
+    public void initializeTransformer() {
+
+        try {
+            final TransformerFactory factory = TransformerFactory.newInstance();
+            factory.setURIResolver(new ClasspathUriResolver(getClass()));
+            final InputStream is = getTemplate();
+            if (is == null) {
+                // identity transformer
+                this.transformer = factory.newTransformer();
+            } else {
+                // template transformer
+                final StreamSource templateSource = new StreamSource(is);
+                final Templates template = factory.newTemplates(templateSource);
+                this.transformer = template.newTransformer();
+            }
+
+        } catch (TransformerConfigurationException | TransformerFactoryConfigurationError
+                | ParserConfigurationException e) {
+            throw new RuntimeException("Could not initialize transformer", e);
+        }
+    }
+
+    protected abstract InputStream getTemplate();
+
+    
+
+    @Override
+    public void writeTo(
+            final Resource paramT,
+            final Class<?> paramClass,
+            final Type paramType,
+            final Annotation[] paramArrayOfAnnotation,
+            final MediaType paramMediaType,
+            final MultivaluedMap<String, Object> paramMultivaluedMap,
+            final OutputStream paramOutputStream)
+            throws IOException, WebApplicationException {
+
+        try {
+            final Source input = new JAXBSource(newJAXBContext(), paramT);
+            final Result result = new StreamResult(paramOutputStream);
+            this.transformer.transform(input, result);
+        } catch (JAXBException | TransformerException e) {
+            LOG.error("Could not produce result", e);
+        }
+
+    }
+
+}
