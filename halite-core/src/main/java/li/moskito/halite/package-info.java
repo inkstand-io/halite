7a2899df65917f90a53fd2fb8fb5e44142ac6db4
Gerald Mücke <gerald.muecke@gmail.com>
2015-05-10 11:28:01 +0200

    Revert "INKPRJ-2 renamed main package"
    This reverts commit d958f2c8a753a5628d574895b833194846fc72e9.

---
+++ halite-core/src/main/java/li/moskito/halite/package-info.java
@@ -0,0 +1,32 @@
+/**
+ * Implementation of the HAL specification.<br>
+ * The core of the HAL implementation are the {@link li.moskito.halite.Resource} and the {@link li.moskito.halite.Link}
+ * classes. Both are compatible for marshalling/unmarshalling with JAXB. To create a custom Resource model, simply
+ * extend the {@link li.moskito.halite.Resource} class or extend the schema types are generate your own classes.
+ * 
+ * Code Example for creating links:<br>
+ * Add a self link to a resource
+ * 
+ * <pre>
+ * Resource res = ...; 
+ * HAL.newLink(res, HAL.NEXT, &quot;http://...&quot;).title(&quot;Next page&quot;);
+ * </pre>
+ * 
+ * Create a new Resource and add a link
+ * 
+ * <pre>
+ * HAL.newResource(&quot;someUri&quot;).addLink(&quot;next&quot;, &quot;page=3&quot;);
+ * </pre>
+ * 
+ * Adding a link to many Resources
+ * 
+ * <pre>
+ * Resource r1 = ...;
+ * Resource r2 = ...;
+ * Resource r3 = ...;
+ * HAL.newLink(&quot;home&quot;, &quot;http://...&quot;).title("Home").addTo(r1, r2, r3);
+ * </pre>
+ */
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://moskito.li/halite")
+package li.moskito.halite;
+
---
+++ halite-rs/src/main/java/li/moskito/halite/rs/ClasspathURIResolver.java
@@ -0,0 +1,102 @@
+package li.moskito.halite.rs;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.URIResolver;
+import javax.xml.transform.stream.StreamSource;
+
+/**
+ * Resolver to resolve URI resources inside the classpath.
+ * @author Gerald Muecke, gerald@moskito.li
+ *
+ */
+public class ClasspathURIResolver implements URIResolver {
+
+    /**
+     * The base class used to resolve the resource if the context classloader is not available
+     */
+    private final Class<?> baseClass;
+    /**
+     * The base path that is preprended to the resource to lookup
+     */
+    private String basePath;
+
+    /**
+     * Creates an {@link URIResolver} that starts searching at the classpath root.
+     * @throws ParserConfigurationException
+     */
+    public ClasspathURIResolver() throws ParserConfigurationException {
+        this(ClasspathURIResolver.class);
+    }
+    
+    /**
+     * Creates an {@link URIResolver} that starts searching at the classpath root
+     * @param base
+     * @throws ParserConfigurationException
+     */
+    public ClasspathURIResolver(String base) throws ParserConfigurationException {
+        this(ClasspathURIResolver.class, base);
+    }
+    
+    /**
+     * Creates an {@link URIResolver} that starts searching at the classpath root
+     * @param base
+     * @throws ParserConfigurationException
+     */
+    public ClasspathURIResolver(final Class<?> base) throws ParserConfigurationException {
+        this(base, "/");
+    }
+    
+    /**
+     * Creates an {@link URIResolver} that starts searching at the given basePath of the classpath
+     * @param base
+     * @param basePath
+     *  the path prefix to resolve resources
+     * @throws ParserConfigurationException
+     */
+    public ClasspathURIResolver(final Class<?> base, String basePath) throws ParserConfigurationException {
+        this.baseClass = base;
+        this.basePath = basePath;
+    }
+
+    @Override
+    public Source resolve(final String href, final String base)
+            throws TransformerException {
+
+        final URL resource = resolve(href);
+        if(resource != null){
+            try {
+                return new StreamSource(resource.openStream());
+            } catch (IOException e) {
+               throw new TransformerException("Could access resource " + href, e);
+            }
+        } else {
+            throw new TransformerException("Could not resolve resource " + href);
+        }
+
+    }
+    
+    /**
+     * Resolves the given name in the classpath.
+     * @param name
+     *  the name of the resource to resolve 
+     * @return
+     *  the URL pointing to the resource
+     */
+    public URL resolve(String name){
+        String fullResourceName = this.basePath + name;
+        final ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        URL url = null;
+        if (cl != null) {
+            url = cl.getResource(fullResourceName);
+        } 
+        if(url == null) {
+            url = baseClass.getResource(fullResourceName);
+        }
+        return url;
+    }
+}
